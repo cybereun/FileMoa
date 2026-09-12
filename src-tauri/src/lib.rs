b30a3471f69edf1cf92311db6e4216a2c978102a
cybereun <cybereunny@gmail.com>
2026-09-12 23:52:39 +0900

    feat: add automatic update check and restart

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -22,6 +22,7 @@ pub fn run() {
         .plugin(tauri_plugin_dialog::init())
         .plugin(tauri_plugin_store::Builder::default().build())
         .plugin(tauri_plugin_updater::Builder::new().build())
+        .plugin(tauri_plugin_process::init())
         .invoke_handler(tauri::generate_handler![plan_organization, execute_organization, find_duplicates])
         .run(tauri::generate_context!())
         .expect("error while running FileMoa");
